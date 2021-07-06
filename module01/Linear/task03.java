07d8170d45883cd0ba2acd50e6d96cc849a60c42
Yury <78310771+kosmykau@users.noreply.github.com>
2021-07-06 12:13:00 +0200

    Update and rename Main10.java to task03.java

--- module01/Linear/task03.java
+++ module01/Linear/task03.java
@@ -1,11 +1,11 @@
 package com.epam.module01.main;
 //3. Вычислить значение выражения по формуле (все переменные принимают 
 //действительные значения): 𝑠𝑖𝑛 𝑥 + 𝑐𝑜𝑠 𝑦 ∗ 𝑡𝑔 𝑥𝑦 𝑐𝑜𝑠 𝑥 − 𝑠𝑖𝑛 𝑦
-public class Main10 {
+public class task03 {
 	
 	public static void main(String[] args) {
 		
-		double x = 10; 
+	double x = 10; 
         double y = 20;
 
         double xRad = x * Math.PI / 180.0;
@@ -16,16 +16,3 @@ public class Main10 {
         System.out.println(result);
     }
 	}
-//		double x;
-//		double y;
-//		double temp;
-//		
-//		x = 10;
-//		y = 20;
-//		
-//		temp = (Math.asin(x) + Math.acos(y));
-//		
-//		System.out.print(temp);		
-//	}	
-//
-//}
