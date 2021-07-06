a0f30775bbde1615ed40194115b2f504d435d813
Yury <78310771+kosmykau@users.noreply.github.com>
2021-07-06 14:03:00 +0200

    Update task04.java

--- module01/Branching/task04.java
+++ module01/Branching/task04.java
@@ -2,18 +2,18 @@ package com.epam.module01.branching;
 //4. Заданы размеры А, В прямоугольного отверстия и размеры х, у, z кирпича. Определить, пройдет ли кирпич через отверстие.
 public class task04 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		
-		int A = 5;
-		int B = 6;
+	int A = 5;
+	int B = 6;
 		
-		int x = 1;
-		int y = 3;
-		int z = 7;
+	int x = 1;
+	int y = 3;
+	int z = 7;
 		
-		if (x <= A && y <= B || y <= A && x <= B || x <= A && z <= B || z <= A && x <= B || z <= A && y <= B || y <= A && z <= B) 
+	if (x <= A && y <= B || y <= A && x <= B || x <= A && z <= B || z <= A && x <= B || z <= A && y <= B || y <= A && z <= B) 
             System.out.println("Кирпич пройдет через отвертие.");    
-		 else 
+	else 
             System.out.println("Кирпич не поройдет через отверстие.");
 			
 	}
