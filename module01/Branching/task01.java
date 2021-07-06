186b79a000c79fc35d5e75d2e26a1f7bdd622ad1
Yury <78310771+kosmykau@users.noreply.github.com>
2021-07-06 14:12:40 +0200

    Update task01.java

--- module01/Branching/task01.java
+++ module01/Branching/task01.java
@@ -2,23 +2,19 @@ package com.epam.module01.branching;
 //Даны два угла треугольника (в градусах). Определить, существует ли такой треугольник, и если да, то будет ли он прямоугольным.
 public class task01 {
 
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 		
-		int corner1 = 45;
-		int corner2 = 45;
+	int corner1 = 45;
+	int corner2 = 45;
 		
-		int corner3 = 180 - corner1 - corner2;
+	int corner3 = 180 - corner1 - corner2;
 		
-		if(corner1 > 0 && corner2 > 0 && corner3 > 0) {
-			System.out.println("Такой треугольник существует");
-			if(corner1 == 90 || corner2 == 90 || corner3 == 90) {
-				System.out.println("Треугольник прямоугольный");
-			}
-			
-		} else {
-			System.out.println("Такого треугольника не существует");
-		}
-		
+	if(corner1 > 0 && corner2 > 0 && corner3 > 0)
+	   System.out.println("Такой треугольник существует");
+	if(corner1 == 90 || corner2 == 90 || corner3 == 90)
+	   System.out.println("Треугольник прямоугольный")
+	else
+	   System.out.println("Такого треугольника не существует");		
 	}
 
 }
