8ae1821d824aeccd0db9663d20cbd3ac47f2f66c
Yury <78310771+kosmykau@users.noreply.github.com>
2021-07-06 14:02:14 +0200

    Update task05.java

--- module01/Branching/task05.java
+++ module01/Branching/task05.java
@@ -3,18 +3,17 @@ package com.epam.module01.branching;
 //                                     1/(x^3 + 6),   если x > 3.}
 public class task05 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		
-		double fx = 0;
-		double x = 2;
+	double fx = 0;
+	double x = 2;
 		
-		if (x <= 3)
-			fx = (Math.pow(x, 2) - (3 * x) + 9);
-		
-		else 
-			fx = (1 / (Math.pow(x, 3) + 6));
+	if (x <= 3)
+           fx = (Math.pow(x, 2) - (3 * x) + 9);	
+	else 
+	   fx = (1 / (Math.pow(x, 3) + 6));
 	
-		System.out.println("F(x) = " +fx);	
+	System.out.println("F(x) = " +fx);	
 	
 	}
 }
