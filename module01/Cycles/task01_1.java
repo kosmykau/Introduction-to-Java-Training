f12085f63ec4f5f9da2e5653d9863ee59e421118
Yury <78310771+kosmykau@users.noreply.github.com>
2021-07-07 14:29:31 +0200

    Add files via upload

---
+++ module01/Cycles/task01_1.java
@@ -0,0 +1,23 @@
+package com.epam.module01.cycles;
+//1. Напишите программу, где пользователь вводит любое целое положительное число. А программа суммируетвсе числа от 1 до введенного пользователем числа.
+import java.util.Scanner;
+public class task01_1 {
+
+	public static void main(String[] args) {
+		
+		int number;
+		int sum = 0;
+		
+		System.out.println("Введите целое положительное число:");
+		Scanner scan = new Scanner(System.in);
+		number = scan.nextInt();
+		
+		for (int i = 1; i <= number; i ++)
+			sum = sum + i;
+		
+		System.out.println("Сумма чисел о 1 до " + number + " равна " + sum);
+		
+
+	}
+
+}
