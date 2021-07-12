294d8d6ea20a07dae0621056c3291b1cb819b2ff
Yury <78310771+kosmykau@users.noreply.github.com>
2021-07-12 11:22:41 +0200

    Add files via upload

---
+++ module01/Cycles/task03.java
@@ -0,0 +1,16 @@
+package com.epam.module01.cycles;
+//3. Найти сумму квадратов первых ста чисел.
+public class task03 {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		
+		for(int i = 1; i <= 100; i++) {
+			sum += i * i;
+		}	
+		
+		System.out.println("Сумма квадратов чисел от 1 до 100 равна " + sum);
+
+	}
+}
